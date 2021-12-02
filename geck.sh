4449fa64cdb76b5c97df9c4e72b05ab0ee797607
AAgzamov <amirkhon.agzamov@gmail.com>
2021-12-02 16:59:22 +0500

    Made geck.s executable only with sudo

--- geck.sh
+++ geck.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/sh
 
-echo "Downloading geckodriver..."
-wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz
-echo "Unarchiving..."
-tar -xvzf geckodriver*
-chmod +x geckodriver
-echo "Moving geckodriver to /usr/local/bin..."
-mv geckodriver /usr/local/bin/
+if [ "$UID" = "0" ];
+then
+
+	echo "Downloading geckodriver..."
+	wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz
+	echo "Unarchiving..."
+	tar -xvzf geckodriver*
+	chmod +x geckodriver
+	echo "Moving geckodriver to /usr/local/bin..."
+	mv geckodriver /usr/local/bin/
+else
+	echo "Run with sudo!"
+fi
